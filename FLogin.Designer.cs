ef6e4d932bc9251347966f968cb2b1da5fdbd972
TrLeQuang <tranquang2005kg@gmail.com>
2024-10-02 21:41:45 +0700

    Add files via upload

---
+++ FLogin.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WinFormsAppCaKoi
+{
+    partial class FLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelLogin = new Panel();
+            btnExit = new Button();
+            btnLogin = new Button();
+            panelPassWordLogin = new Panel();
+            txbPassWord = new TextBox();
+            textBox3 = new TextBox();
+            panelUserNameLogin = new Panel();
+            txbUserName = new TextBox();
+            textBox1 = new TextBox();
+            panelLogin.SuspendLayout();
+            panelPassWordLogin.SuspendLayout();
+            panelUserNameLogin.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            panelLogin.Controls.Add(btnExit);
+            panelLogin.Controls.Add(btnLogin);
+            panelLogin.Controls.Add(panelPassWordLogin);
+            panelLogin.Controls.Add(panelUserNameLogin);
+            panelLogin.Location = new Point(12, 12);
+            panelLogin.Name = "panelLogin";
+            panelLogin.Size = new Size(776, 426);
+            panelLogin.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(615, 245);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(142, 46);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(297, 245);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(142, 46);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // panelPassWordLogin
+            // 
+            panelPassWordLogin.Controls.Add(txbPassWord);
+            panelPassWordLogin.Controls.Add(textBox3);
+            panelPassWordLogin.Location = new Point(0, 143);
+            panelPassWordLogin.Name = "panelPassWordLogin";
+            panelPassWordLogin.Size = new Size(770, 96);
+            panelPassWordLogin.TabIndex = 1;
+            // 
+            // txbPassWord
+            // 
+            txbPassWord.Location = new Point(286, 34);
+            txbPassWord.Name = "txbPassWord";
+            txbPassWord.Size = new Size(481, 31);
+            txbPassWord.TabIndex = 1;
+            txbPassWord.UseSystemPasswordChar = true;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.MenuBar;
+            textBox3.Font = new Font("Arial", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.ForeColor = SystemColors.WindowText;
+            textBox3.Location = new Point(13, 24);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(258, 44);
+            textBox3.TabIndex = 0;
+            textBox3.Text = "Mật khẩu:";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // panelUserNameLogin
+            // 
+            panelUserNameLogin.Controls.Add(txbUserName);
+            panelUserNameLogin.Controls.Add(textBox1);
+            panelUserNameLogin.Location = new Point(3, 17);
+            panelUserNameLogin.Name = "panelUserNameLogin";
+            panelUserNameLogin.Size = new Size(770, 102);
+            panelUserNameLogin.TabIndex = 0;
+            // 
+            // txbUserName
+            // 
+            txbUserName.Location = new Point(283, 38);
+            txbUserName.Name = "txbUserName";
+            txbUserName.Size = new Size(481, 31);
+            txbUserName.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.MenuBar;
+            textBox1.Font = new Font("Arial", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.WindowText;
+            textBox1.Location = new Point(10, 28);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(258, 44);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Tên đăng nhập:";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // FLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelLogin);
+            Name = "FLogin";
+            Text = "Đăng nhập";
+            panelLogin.ResumeLayout(false);
+            panelPassWordLogin.ResumeLayout(false);
+            panelPassWordLogin.PerformLayout();
+            panelUserNameLogin.ResumeLayout(false);
+            panelUserNameLogin.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelLogin;
+        private Panel panelUserNameLogin;
+        private TextBox textBox1;
+        private Panel panelPassWordLogin;
+        private TextBox txbPassWord;
+        private TextBox textBox3;
+        private TextBox txbUserName;
+        private Button btnLogin;
+        private Button btnExit;
+    }
+}
---
+++ fSignUp.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WinFormsAppCaKoi
+{
+    partial class fSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelSignUp = new Panel();
+            panelUserNameSignUp = new Panel();
+            textBox1 = new TextBox();
+            txbUserNameSignUp = new TextBox();
+            panelPassWordSignUp = new Panel();
+            txbPassWordSignUp = new TextBox();
+            textBox3 = new TextBox();
+            btnSignUp = new Button();
+            btnExitSignUp = new Button();
+            panelSignUp.SuspendLayout();
+            panelUserNameSignUp.SuspendLayout();
+            panelPassWordSignUp.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelSignUp
+            // 
+            panelSignUp.Controls.Add(btnExitSignUp);
+            panelSignUp.Controls.Add(btnSignUp);
+            panelSignUp.Controls.Add(panelPassWordSignUp);
+            panelSignUp.Controls.Add(panelUserNameSignUp);
+            panelSignUp.Location = new Point(5, 7);
+            panelSignUp.Name = "panelSignUp";
+            panelSignUp.Size = new Size(786, 430);
+            panelSignUp.TabIndex = 0;
+            // 
+            // panelUserNameSignUp
+            // 
+            panelUserNameSignUp.Controls.Add(txbUserNameSignUp);
+            panelUserNameSignUp.Controls.Add(textBox1);
+            panelUserNameSignUp.Location = new Point(7, 5);
+            panelUserNameSignUp.Name = "panelUserNameSignUp";
+            panelUserNameSignUp.Size = new Size(771, 84);
+            panelUserNameSignUp.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(17, 23);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(277, 44);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Tên đăng nhập:";
+            // 
+            // txbUserNameSignUp
+            // 
+            txbUserNameSignUp.Location = new Point(316, 33);
+            txbUserNameSignUp.Name = "txbUserNameSignUp";
+            txbUserNameSignUp.Size = new Size(452, 31);
+            txbUserNameSignUp.TabIndex = 1;
+            // 
+            // panelPassWordSignUp
+            // 
+            panelPassWordSignUp.Controls.Add(txbPassWordSignUp);
+            panelPassWordSignUp.Controls.Add(textBox3);
+            panelPassWordSignUp.Location = new Point(7, 108);
+            panelPassWordSignUp.Name = "panelPassWordSignUp";
+            panelPassWordSignUp.Size = new Size(771, 84);
+            panelPassWordSignUp.TabIndex = 1;
+            // 
+            // txbPassWordSignUp
+            // 
+            txbPassWordSignUp.Location = new Point(316, 33);
+            txbPassWordSignUp.Name = "txbPassWordSignUp";
+            txbPassWordSignUp.Size = new Size(452, 31);
+            txbPassWordSignUp.TabIndex = 1;
+            txbPassWordSignUp.UseSystemPasswordChar = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Arial", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(17, 23);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(277, 44);
+            textBox3.TabIndex = 0;
+            textBox3.Text = "Mật khẩu:";
+            // 
+            // btnSignUp
+            // 
+            btnSignUp.Location = new Point(323, 207);
+            btnSignUp.Name = "btnSignUp";
+            btnSignUp.Size = new Size(171, 53);
+            btnSignUp.TabIndex = 2;
+            btnSignUp.Text = "Đăng ký";
+            btnSignUp.UseVisualStyleBackColor = true;
+            btnSignUp.Click += btnSignUp_Click;
+            // 
+            // btnExitSignUp
+            // 
+            btnExitSignUp.Location = new Point(607, 207);
+            btnExitSignUp.Name = "btnExitSignUp";
+            btnExitSignUp.Size = new Size(171, 53);
+            btnExitSignUp.TabIndex = 3;
+            btnExitSignUp.Text = "Thoát";
+            btnExitSignUp.UseVisualStyleBackColor = true;
+            btnExitSignUp.Click += btnExitSignUp_Click;
+            // 
+            // fSignUp
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelSignUp);
+            Name = "fSignUp";
+            Text = "Tạo tài khoản";
+            panelSignUp.ResumeLayout(false);
+            panelUserNameSignUp.ResumeLayout(false);
+            panelUserNameSignUp.PerformLayout();
+            panelPassWordSignUp.ResumeLayout(false);
+            panelPassWordSignUp.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelSignUp;
+        private Panel panelUserNameSignUp;
+        private TextBox txbUserNameSignUp;
+        private TextBox textBox1;
+        private Button btnExitSignUp;
+        private Button btnSignUp;
+        private Panel panelPassWordSignUp;
+        private TextBox txbPassWordSignUp;
+        private TextBox textBox3;
+    }
+}
