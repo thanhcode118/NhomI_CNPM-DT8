3f3689ff1faef014bab1da2c06c39a83648e7fbf
Em Riz <150053572+maileminhtrinh2005@users.noreply.github.com>
2024-10-04 21:01:46 +0700

    Add files via upload

---
+++ quanlysk.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using System;
+
+public partial class FormQuanLySuKien : Form
+{
+    public FormQuanLySuKien()
+    {
+        InitializeComponent();
+    }
+
+    private void btnTaoSuKien_Click(object sender, EventArgs e)
+    {
+        string tenSuKien = txtTenSuKien.Text;
+        DateTime thoiGian = dtpThoiGian.Value;
+        string diaDiem = txtDiaDiem.Text;
+        string hinhThuc = rbtnOnline.Checked ? "Online" : "Offline";
+
+        MessageBox.Show("Tạo sự kiện thành công!");
+    }
+
+    private void btnSuaSuKien_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void btnXoaSuKien_Click(object sender, EventArgs e)
+    {
+        // Chức năng xóa sự kiện đã tạo
+    }
+}
