0ae65359749c7955f48d624ccd4faa8b40b4ffc5
TranVanVu <166126091+TranVanVu2502@users.noreply.github.com>
2024-10-21 23:01:50 +0700

    ok

---
+++ NhomI_CNPM-DT8-5d36970cdd308c16f894638f28cc6e289fd664ab/KOI Competition/KOI Competition/Data/KOI_CompetitionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KOI_Competition.Models;
+
+namespace KOI_Competition.Data
+{
+    public class KOI_CompetitionContext : DbContext
+    {
+        public KOI_CompetitionContext (DbContextOptions<KOI_CompetitionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KOI_Competition.Models.User> User { get; set; } = default!;
+    }
+}
