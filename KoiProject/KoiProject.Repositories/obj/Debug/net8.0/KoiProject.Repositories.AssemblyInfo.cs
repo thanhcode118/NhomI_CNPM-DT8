ffe5e1b9f0e99ad3ebd9303672fd5ccd644140fc
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-10-25 08:42:46 -0700

    Thanh xoa nut Dang ki, push len branch Thanh_Leaader

--- KoiProject/KoiProject.Repositories/obj/Debug/net8.0/KoiProject.Repositories.AssemblyInfo.cs
+++ KoiProject/KoiProject.Repositories/obj/Debug/net8.0/KoiProject.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfdcb1f311bd839b37d17e1664b616f3d744a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KoiProject/KoiProject.WebApplication/wwwroot/js/site.js
+++ KoiProject/KoiProject.WebApplication/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let currentIndex = 0;
 
-// Write your JavaScript code.
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    const slideTrack = document.querySelector('.slide-track');
+    currentIndex++;
+
+    // Reset về slide đầu tiên khi đến slide cuối
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    // Dịch chuyển slide track để hiển thị slide hiện tại
+    const translateXValue = -currentIndex * 100; // Tính khoảng cách dịch chuyển theo phần trăm
+    slideTrack.style.transform = `translateX(${translateXValue}%)`;
+
+    // Tự động chuyển slide mỗi 3 giây
+    setTimeout(showSlides, 3000);
+}
+
+// Bắt đầu chuyển slide khi trang được tải
+window.onload = function () {
+    showSlides();
+};
