dd3031460c6c1b08149a02e0dbd20521c71dd872
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-10-27 06:51:39 -0700

    Them form HomePage

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e041a515fbd290720add5f16955a1a38c8c62976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6126b4e0dfb783eecee3b5cd3e6338080c9b0af7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KoiProject/KoiProject.WebApplication/wwwroot/lib/jquery-validation/LICENSE.md
+++ KoiProject/KoiProject.WebApplication/wwwroot/lib/jquery-validation/LICENSE.md
@@ -1,5 +1,6 @@
 The MIT License (MIT)
-=====================
+
+================
 
 Copyright Jörn Zaefferer
 
