e771163d77f038f824c931315896837f13d02564
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-10-26 19:28:27 -0700

    Thanh xoa cac chuc nang khong lien quan

--- KoiProject/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
+++ KoiProject/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c58d0c7f20787513e564b157c8b169ce0db3d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6bd6bfaf35e1aff2dcf05de5b732cf20fa38ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
