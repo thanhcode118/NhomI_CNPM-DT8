e5b57eced940ad1dcf1aa6f122ce1a4eea729eac
Nguyen Ngoc Toan <ngoctoan24042005@gmail.com>
2024-10-27 18:05:35 +0700

    Hoan Thanh Giao Dien web mot cach xinh dep

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e771163d77f038f824c931315896837f13d02564")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ba35b8c4666bbc6f0116b021176123a357aac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
