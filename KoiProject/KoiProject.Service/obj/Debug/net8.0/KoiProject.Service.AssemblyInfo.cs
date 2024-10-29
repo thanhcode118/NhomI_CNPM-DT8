845bb7d207cd13018b3ce5d86800702b9b4ed4f7
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-10-29 09:29:13 -0700

    Thanh them dashboard va sua login, regis form

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fdc535d8246529400602f492c1ad901e7acf48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1993ccc0e0025caab568d773bb64c20909dd498a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
