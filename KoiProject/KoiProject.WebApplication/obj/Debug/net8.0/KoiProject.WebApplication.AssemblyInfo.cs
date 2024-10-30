c277f7654141764d61c02f7a36d8d410667bbd74
Nguyen Ngoc Toan <ngoctoan24042005@gmail.com>
2024-10-30 19:03:05 +0700

    Design overview cua trang Dashboard

--- KoiProject/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
+++ KoiProject/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0768d33e9518368016d54bbc6017ebb400a3f8f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6083defa6a5a9910440888afe986783ce8841db8")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
