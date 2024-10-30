df1fe1939da2b2c61c2eecbe4ad41dc58c8e0b7e
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-10-29 21:03:12 -0700

    Thanh them database script

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6a0ac65957ca575f28b182139feefbc4e94403")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7b3834e04054b9fdcc4bf3a7402803e1ea39a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
