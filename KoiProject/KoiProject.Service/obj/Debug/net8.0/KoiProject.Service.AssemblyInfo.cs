00ebde6b15c0947f28bc291c1c10453cd30d003b
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-11-09 20:17:17 -0700

    Thanh up templates contest detail

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5f4a87b623db6f17b7f206c5928f10b3c13692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef53d09071f4c4a4b0ed75853441928b9911803")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
