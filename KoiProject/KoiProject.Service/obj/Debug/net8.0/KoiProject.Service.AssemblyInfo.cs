4f9e67b704dd48d6fb819b71ae65c455bf223f6a
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-11-14 07:10:26 -0700

    Thanh fix

--- KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProject/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5172e682810331013a9e5866061f23cf489998e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3993c42928f36744f69527538f4c23840b611947")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
