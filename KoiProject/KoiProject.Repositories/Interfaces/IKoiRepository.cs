fbc47fd64362de7f6c242ff59729dd411ed804c8
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2024-11-19 08:34:15 -0700

    Thanh hoan thanh cuoc thi va hien thi Ranking GPA

---
+++ KoiProject/KoiProject.Repositories/Interfaces/IKoiRepository.cs
@@ -0,0 +1,12 @@
+﻿using KoiProject.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IKoiRepository
+{
+    void RandomizeScores();
+    List<KoiManagement> GetRanking();
+}
---
+++ KoiProject/KoiProject.Service/Service/KoiServiceCompetition.cs
@@ -0,0 +1,26 @@
+﻿using KoiProject.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class KoiServiceCompetition : IKoiServiceCompetition
+{
+    private readonly IKoiRepository _koiRepository;
+
+    public KoiServiceCompetition(IKoiRepository koiRepository)
+    {
+        _koiRepository = koiRepository;
+    }
+
+    public void GenerateRandomScores()
+    {
+        _koiRepository.RandomizeScores();
+    }
+
+    public List<KoiManagement> GetRanking()
+    {
+        return _koiRepository.GetRanking();
+    }
+}
